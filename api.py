b2db50eb6667cedd3a02f9548784c552627ef9e7
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-05-31 21:04:03 -0300

    Structure folders and files

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import config
 from routes.sentenceRouter import sentence_blueprint
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(sentence_blueprint, url_prefix='/sentence')
 
