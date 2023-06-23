ce12aa582a8b13cd7327b75913e1a5ec10fa3dd1
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-06-23 19:48:39 -0300

    Company CRUD methods, Company grade default value

--- routes/companyRouter.py
+++ routes/companyRouter.py
@@ -24,3 +24,60 @@ def get_all_companies():
         }
         result.append(company_data)
     return jsonify(result)
+
+@app.route('/company/<int:company_id>', methods=['GET'])
+def get_company_by_id(company_id):
+    company = Company.query.get(company_id)
+
+    if company:
+        company_data = {
+            'id': company.id,
+            'name': company.name,
+            'visibility': company.visibility,
+            'grade': company.grade
+        }
+        return jsonify(company_data)
+    else:
+        return jsonify({'error': 'Company not found'}), 404
+    
+@app.route('/company/search/<string:name>', methods=['GET'])
+def get_user_by_name(name):
+    company = Company.query.filter_by(name=name).first()
+
+    if company:
+        company_data = {
+            'id': company.id,
+            'name': company.name,
+            'visibility': company.visibility,
+            'grade': company.grade
+        }
+        return jsonify(company_data)
+    else:
+        return jsonify({'error': 'company not found'})
+
+
+@app.route('/company/<int:company_id>', methods=['PUT'])
+def update_company_info(company_id):
+    company = Company.query.get(company_id)
+
+    if company:
+        data = request.get_json()
+        company.name = data.get('name', company.name)
+        company.visibility = data.get('visibility', company.visibility)
+
+        db.session.commit()
+
+        return jsonify({'message': 'Company information updated successfully'})
+    else:
+        return jsonify({'error': 'Company not found'}), 404
+
+@app.route('/company/<int:company_id>', methods=['DELETE'])
+def delete_company(company_id):
+    company = Company.query.get(company_id)
+
+    if company:
+        db.session.delete(company)
+        db.session.commit()
+        return jsonify({'message': 'Company deleted successfully'})
+    else:
+        return jsonify({'error': 'Company not found'}), 404
