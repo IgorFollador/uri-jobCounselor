7f74e2fa4dbc2fcfe52bf1c376d6c7f0c20957f0
Igor Follador <igorledf@hotmail.com>
2023-06-23 21:16:14 -0300

    Add CORS correction

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_pyfile('./config/config.py')
-
+CORS(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
