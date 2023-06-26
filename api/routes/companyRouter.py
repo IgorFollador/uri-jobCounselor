1a76111d0446156cb4f4634900a9d1f6f6b6b9e4
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-06-26 00:40:53 -0300

    Separating methods and routes

--- api/routes/companyRouter.py
+++ api/routes/companyRouter.py
@@ -1,83 +1,49 @@
-from flask import request, Response, jsonify
-from models.companies import Company
-from app import app, db
+from flask import request, jsonify
+from services.companyService import *
+from app import app
 
 @app.route('/company', methods=['POST'])
 def create_company():
     data = request.get_json()
-    new_company = Company(name=data['name'], 
-                          visibility=data['visibility'])
-    db.session.add(new_company)
-    db.session.commit()
+    create_new_company(data)
     return jsonify({'message': 'Company created successfully!'})
 
 @app.route('/companies', methods=['GET'])
 def get_all_companies():
-    companies = Company.query.all()
-    result = []
-    for company in companies:
-        company_data = {
-            'id': company.id,
-            'name': company.name,
-            'visibility': company.visibility,
-            'grade': company.grade   
-        }
-        result.append(company_data)
-    return jsonify(result)
+    companies = get_companies()
+    return jsonify(companies)
 
 @app.route('/company/<int:company_id>', methods=['GET'])
 def get_company_by_id(company_id):
-    company = Company.query.get(company_id)
-
+    company = get_specific_company(company_id)
     if company:
-        company_data = {
-            'id': company.id,
-            'name': company.name,
-            'visibility': company.visibility,
-            'grade': company.grade
-        }
-        return jsonify(company_data)
+        return jsonify(company)
     else:
         return jsonify({'error': 'Company not found'}), 404
-    
+
 @app.route('/company/search/<string:name>', methods=['GET'])
-def get_user_by_name(name):
-    company = Company.query.filter_by(name=name).first()
-
+def get_company_by_name(name):
+    company = search_company_by_name(name)
     if company:
-        company_data = {
-            'id': company.id,
-            'name': company.name,
-            'visibility': company.visibility,
-            'grade': company.grade
-        }
-        return jsonify(company_data)
+        return jsonify(company)
     else:
-        return jsonify({'error': 'company not found'})
-
+        return jsonify({'error': 'Company not found'})
 
 @app.route('/company/<int:company_id>', methods=['PUT'])
 def update_company_info(company_id):
-    company = Company.query.get(company_id)
-
+    company = get_specific_company(company_id)
     if company:
         data = request.get_json()
-        company.name = data.get('name', company.name)
-        company.visibility = data.get('visibility', company.visibility)
-
-        db.session.commit()
-
+        update_company(company_id, data)
         return jsonify({'message': 'Company information updated successfully'})
     else:
         return jsonify({'error': 'Company not found'}), 404
 
 @app.route('/company/<int:company_id>', methods=['DELETE'])
-def delete_company(company_id):
-    company = Company.query.get(company_id)
-
+def delete_company_by_id(company_id):
+    company = get_specific_company(company_id)
     if company:
-        db.session.delete(company)
-        db.session.commit()
+        delete_company(company_id)
         return jsonify({'message': 'Company deleted successfully'})
     else:
         return jsonify({'error': 'Company not found'}), 404
